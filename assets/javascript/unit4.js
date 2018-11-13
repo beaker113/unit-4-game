af45d11e376938d77f564d337d322a13d79dff3e
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-11-13 15:43:20 -0800

    added html, css, starting java

--- assets/javascript/unit4.js
+++ assets/javascript/unit4.js
@@ -1,4 +1,58 @@
-$("#random-button").on("click", function() {
-    var randomNum = Math.floor(Math.random() * 1000) + 1; 
-    $("#random-number").text(randomNum);
+$(document).ready(function() {
+
+  var randomNumber = Math.floor((Math.random() * 120) + 19);
+console.log(randomNumber)
+  
+
+  var crystal1 = Math.floor((Math.random() * 12) + 1);
+
+  var crystal2 = Math.floor((Math.random() * 12) + 1);
+
+  var crystal3 = Math.floor((Math.random() * 12) + 1);
+
+  var crystal4 = Math.floor((Math.random() * 12) + 1);
+
+  var score = 0;
+
+  var wins = 0;
+  
+  var loss = 0;
+
+
+
+
+$("#rand").append(randomNumber); 
+
+  $("#crystal1").on("click", function() {
+     score + crystal1;
+     $("#score").append(score);
   });
+    
+  
+$("#crystal2").on("click", function() {
+  score + crystal2;
+  $("#score").append(score);
+});
+
+$("#crystal3").on("click", function() {
+  score + crystal3;
+  $("#score").append(score);
+});
+$("#crystal4").on("click", function() {
+  score + crystal4;
+  $("#score").append(score);
+});
+
+
+if (score === randomNumber) {
+  wins++;
+}
+
+if(score > randomNumber) {
+  loss++;
+}
+   
+   
+
+ 
+});
