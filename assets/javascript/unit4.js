c53f6ff7fd70bcacbfa9a95e97ab2505839048ac
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-11-17 11:12:18 -0800

    fixed some functions

--- assets/javascript/unit4.js
+++ assets/javascript/unit4.js
@@ -1,58 +1,71 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   var randomNumber = Math.floor((Math.random() * 120) + 19);
-console.log(randomNumber)
-  
+  console.log(randomNumber)
 
-  var crystal1 = Math.floor((Math.random() * 12) + 1);
 
-  var crystal2 = Math.floor((Math.random() * 12) + 1);
-
-  var crystal3 = Math.floor((Math.random() * 12) + 1);
-
-  var crystal4 = Math.floor((Math.random() * 12) + 1);
 
   var score = 0;
-
   var wins = 0;
+  var loss = 0; 
   
-  var loss = 0;
+  
+
+  var crystalImgPaths = ["assets/images/crystal1.jpg", "assets/images/crystal2.png", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"]
 
 
+  for (var i = 0; i < crystalImgPaths.length; i++) {
 
-
-$("#rand").append(randomNumber); 
-
-  $("#crystal1").on("click", function() {
-     score + crystal1;
-     $("#score").append(score);
-  });
+    var randomValue = Math.floor((Math.random() * 12) + 1);
+    var crystal = $("<img>");
+    crystal.attr("src", crystalImgPaths[i]);
+    crystal.attr("class", 'crystals');
+    crystal.attr("value", randomValue);
+    crystal.attr("data-id", randomValue);
+    var imgTarget = $("#crystals");
+    imgTarget.append(crystal);
     
+
+  }
+   
+  $(".crystals").on("click", function(e){
+      
+     
+      score += Number($(this).data("id"));
+    
+      
+      $("#points").text(score);
+
+      if(score === randomNumber){
+        wins++;
+        $("#wins").text(wins);
+      }
+
+      if(score > randomNumber) {
+        loss++;
+        $("#losses").text(loss);
+        score = 0;
+       
+        location.reload();
+        
+      }
+    
+  });
+
+
+
+
+
+
+
+
+
+
+  $("#rand").append(randomNumber);
+
   
-$("#crystal2").on("click", function() {
-  score + crystal2;
-  $("#score").append(score);
-});
-
-$("#crystal3").on("click", function() {
-  score + crystal3;
-  $("#score").append(score);
-});
-$("#crystal4").on("click", function() {
-  score + crystal4;
-  $("#score").append(score);
-});
+  
+  
 
 
-if (score === randomNumber) {
-  wins++;
-}
-
-if(score > randomNumber) {
-  loss++;
-}
-   
-   
-
- 
 });
