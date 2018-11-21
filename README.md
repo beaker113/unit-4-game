1a6496969532e285a8739fe19d616524d241e297
beaker113 <43103384+beaker113@users.noreply.github.com>
2018-11-20 21:41:11 -0800

    works when crystals change

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # unit-4-game
+This is a simple game created using Jquery.
+The crystals on the page are dynamically added to the html and uses a for loop that
+assigns each one a random value each time the player wins and loses.
+The goal number is between 1 and 120 using the math.random method.
+if statements are used to determine if the player has met the goal number resulting in a win or if the player has gone over the goal number resulting in a loss.
--- assets/javascript/unit4.js
+++ assets/javascript/unit4.js
@@ -1,78 +1,69 @@
 $(document).ready(function () {
 
-  var start = function() {
-randomNumber = Math.floor((Math.random() * 120) + 19);
-}
+  var start = function () {
+    randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+  }
+
 
 
 
   var score = 0;
   var wins = 0;
-  var loss = 0; 
-  
-  
+  var loss = 0;
+
+
 
   var crystalImgPaths = ["assets/images/crystal1.jpg", "assets/images/crystal2.png", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"]
+  
+  var handleCrystalClick = function (e) {
+    score += Number($(this).data("id"));
 
+    $("#points").text(score);
 
-  for (var i = 0; i < crystalImgPaths.length; i++) {
+    if (score === randomNumber) {
+      wins++;
+      $("#wins").text(wins);
+      score = 0;
+      $("#crystals").empty();
+      start();
+      newCrystals();
+      $("#rand").text(randomNumber);
+    };
 
-    var randomValue = Math.floor((Math.random() * 12) + 1);
-    var crystal = $("<img>");
-    crystal.attr("src", crystalImgPaths[i]);
-    crystal.attr("class", 'crystals');
-    crystal.attr("value", randomValue);
-    crystal.attr("data-id", randomValue);
-    var imgTarget = $("#crystals");
-    imgTarget.append(crystal);
-    
+    if (score > randomNumber) {
+      loss++;
+      $("#losses").text(loss);
+      score = 0;
+      $("#crystals").empty();
+      start();
+      newCrystals();
+      $("#rand").text(randomNumber);
 
+    };
   }
 
+  var newCrystals = function () {
+    for (var i = 0; i < crystalImgPaths.length; i++) {
+
+      var randomValue = Math.floor((Math.random() * 12) + 1);
+      var crystal = $("<img>");
+      crystal.attr("src", crystalImgPaths[i]);
+      crystal.attr("class", 'crystals');
+      crystal.attr("value", randomValue);
+      crystal.attr("data-id", randomValue);
+      var imgTarget = $("#crystals");
+      imgTarget.append(crystal);
+    }
+    $(".crystals").on("click", handleCrystalClick);
+  }
+
+  newCrystals();
   start();
+
   $("#rand").text(randomNumber);
-   
-  $(".crystals").on("click", function(e){
-      
-     
-      score += Number($(this).data("id"));
-    
-      
-      $("#points").text(score);
 
-      if(score === randomNumber){
-        wins++;
-        $("#wins").text(wins);
+  // $(".crystals").on("click", handleCrystalClick);
 
-        start();
-        $("#rand").text(randomNumber);
-      };
-
-      if(score > randomNumber) {
-        loss++;
-        $("#losses").text(loss);
-        score = 0;
-       
-        start();
-        $("#rand").text(randomNumber);
-      };
-    
-  });
-
-
-
-
-
-
-
-
-
-
-  
-
-  
-  
-  
 
 
 });
